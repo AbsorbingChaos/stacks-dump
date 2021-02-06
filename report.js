7ca8041e06e4b3925551a45c56507bee60b95a72
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-02-06 10:11:21 -0700

    remove block reward due to consistent 0 value

--- report.js
+++ report.js
@@ -876,7 +876,7 @@ function process_burnchain_ops() {
       .addField('Total Miners (last block)', miner_count_last_block, true)
       .addField('Total Miners (overall)', Object.keys(miners).length)
       .addField('Total Commit (last block)', numberWithCommas(burn_last_block, 0))
-      .addField('Block Reward (last block)', numberWithCommas(reward_last_block, 0))
+      // .addField('Block Reward (last block)', numberWithCommas(reward_last_block, 0))
       .addField('Active Miners (' + miner_count + ')', miner_list)
       .setColor('#5546FF')
       .setThumbnail('https://stacks101-com.chaos.workers.dev/img/stacks-mine.png')
